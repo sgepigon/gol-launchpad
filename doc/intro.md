3d7f30e45a6e3e4483c032d0e6b75b1b7605e072
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-28 19:34:15 -0400

    Init lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gol-launchpad
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject gol-launchpad "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[expound "0.7.1"]
+                 [org.clojure/clojure "1.10.0-alpha6"]
+                 [org.clojure/test.check "0.9.0"]
+                 [overtone "0.10.3"]])
