42143ab93c0cfd033540a2ccded6b33796e101ab
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-29 11:22:59 -0400

    Add the Game of Life rules as specs

--- src/gol_launchpad/core.clj
+++ src/gol_launchpad/core.clj
@@ -50,6 +50,41 @@
        (filter live?)
        count))
 
+;; Game of Life rules
+
+;; Any live cell with fewer than two live neighbors dies
+(s/def ::underpopulation (s/and #(live? (apply cell %))
+                                #(< (apply neighbors %) 2)))
+
+;; Any live cell with two or three live neighbors survives
+(s/def ::survive (s/and #(live? (apply cell %))
+                        #(#{2 3} (apply neighbors %))))
+
+;; Any live cell with more than three live neighbors dies
+(s/def ::overpopulation (s/and #(live? (apply cell %))
+                               #(> (apply neighbors %) 3)))
+
+;; Any dead cell with exactly three live neighbors becomes a live cell
+(s/def ::reproduction (s/and #(dead? (apply cell %))
+                             #(= (apply neighbors %) 3)))
+
+(s/fdef rules
+  :args (s/cat :board ::board, :coords ::coords)
+  :ret ::cell)
+(defn rules
+  "Update a cell on `board` according to the Game of Life rules:
+
+  1. Any live cell with fewer than two live neighbors dies
+  2. Any live cell with two or three live neighbors survives
+  3. Any live cell with more than three live neighbors dies
+  4. Any dead cell with exactly three live neighbors becomes a live cell."
+  [board [row col :as coords]]
+  (cond
+    (s/valid? ::underpopulation [board coords]) dead
+    (s/valid? ::survive [board coords]) live
+    (s/valid? ::overpopulation [board coords]) dead
+    (s/valid? ::reproduction [board coords]) live
+    :else (cell board coords)))
 ;; midi
 
 (def lp (first (midi-connected-receivers)))
