ac1b507d7844e4f8dbab519c03238c68510a7663
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-30 16:29:49 -0400

    [WIP] Add second pairing session

--- src/gol_launchpad/core.clj
+++ src/gol_launchpad/core.clj
@@ -7,27 +7,43 @@
             [gol-launchpad.patterns :as patterns]
             [overtone.live :as overtone :refer :all]))
 
+(event-debug-on)
+(on-event [:midi :note-on]
+          (fn [{:keys [note]}]
+            (do (print note)
+                (swap! #(toggle-cell % (midi-note->coords note)) board)))
+          ::lp-handler)
+
+(def board (atom patterns/barren))
+
 (def colors {:green 120})
 
 ;; midi
 
 (def lp (first (midi-connected-receivers)))
 
-(s/fdef ->midi-note
+(s/fdef coords->midi-note
   :args (s/cat :coords ::life/coords)
   :ret nat-int?)
-(defn- ->midi-note
+(defn- coords->midi-note
   "Translate `coords` to its place on the Launchpad."
   [[row col :as coords]]
   (+ (* 16 row) col))
 
+(s/fdef midi-note->coords
+  :args (s/cat :midi-note nat-int?)
+  :ret ::life/coords)
+(defn- midi-note->coords
+  [midi-note]
+  (apply (juxt quot rem) [midi-note 16]))
+
 (defn- toggle-on
   [[row col :as coords]]
-  (midi-note-on lp (->midi-note coords) (:green colors)))
+  (midi-note-on lp (coords->midi-note coords) (:green colors)))
 
 (defn- toggle-off
   [[row col :as coords]]
-  (midi-note-off lp (->midi-note coords)))
+  (midi-note-off lp (coords->midi-note coords)))
 
 (defn- toggle-cell
   [board [row col :as coords]]
@@ -44,10 +60,29 @@
   "TODO"
   [m]
   (let [beat (m)]
-    (do (println beat)
+    (do (print-board @board)
+        (swap! board life/next)
         (apply-by (m (inc beat)) #'main-loop [m]))))
 
+
+
+
 (comment
+
+
+  (reset! board [])
+
+  (main-loop (metronome 120))
+  (stop)
+
+  @board
+
+
+  (doseq [board (take 5 (iterate life/next patterns/blinker))] (print-board board))
+
+  (print-board patterns/barren)
+
+  (stop)
 
   (set! s/*explain-out* (expound/custom-printer {:show-valid-values? true
                                                  :print-specs? false
