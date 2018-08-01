f2a3809d3db5cd4b38f00b9eecdfb2ec0abcb026
Daniel Friedman <dfriedman58@gmail.com>
2018-08-01 18:20:57 -0400

    Sound better

--- src/gol_launchpad/sound.clj
+++ src/gol_launchpad/sound.clj
@@ -14,23 +14,29 @@
   [tonic]
   (take 8 (iterate p5 tonic)))
 
-(def ^:private root 30)
-
 (def ^:private lookup-table
-  (vector
-    (vec (make-row root))
-    (vec (make-row (p4 root)))
-    (vec (make-row (p4 (p4 root))))
-    (vec (make-row (p4 (p4 (p4 root)))))
-    (vec (make-row (p4 (p4 (p4 (p4 root))))))
-    (vec (make-row (p4 (p4 (p4 (p4 (p4 root)))))))
-    (vec (make-row (p4 (p4 (p4 (p4 (p4 (p4 root))))))))
-    (vec (make-row (p4 (p4 (p4 (p4 (p4 (p4 (p4 root)))))))))))
+  (let [root 50]
+    (vector
+      (vec (make-row root))
+      (vec (make-row (p4 root)))
+      (vec (make-row (p4 (p4 root))))
+      (vec (make-row (p4 (p4 (p4 root)))))
+      (vec (make-row (p4 (p4 (p4 (p4 root))))))
+      (vec (make-row (p4 (p4 (p4 (p4 (p4 root)))))))
+      (vec (make-row (p4 (p4 (p4 (p4 (p4 (p4 root))))))))
+      (vec (make-row (p4 (p4 (p4 (p4 (p4 (p4 (p4 root))))))))))))
+
+(defsynth s [freq 440]
+          (let [factor 4
+                mul 20
+                env (perc)
+                sig (* 0.5 (* (env-gen env :action FREE) (sin-osc (+ freq (* mul (sin-osc (* freq factor)))))))]
+            (out 0 [sig sig])))
 
 (defn play
   [board coords]
   (when (life/live? (life/cell board coords))
-    (demo (sin-osc (get-in lookup-table coords)))))
+    (s (get-in lookup-table coords))))
 
 (defn play-board
   [board]
